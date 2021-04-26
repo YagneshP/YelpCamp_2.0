f53d00ab0ba5f4cee3f7f4acea00d2c54d004072
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-04-26 16:44:43 -0400

    feat: create campground model and index route for campgrounds

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express')
-const app = express()
+const app 		= express()
+const path   	= require('path')
+const mongoose = require('mongoose')
+const Campground = require('./model/campground')
 
+// mongoose.connect()
+
+
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname, 'views'));
 
 app.get("/",(req,res)=>{
-	res.send("the future home page for the app")
+	res.render('home')
+})
+
+app.get('/campgrounds', async (req,res)=>{
+	// const campgrounds = await Campground.find({})
+	res.render('campgrounds/index')
 })
 
 //Listening server

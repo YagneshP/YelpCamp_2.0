3a84b6a82a784fe6e8a9b5a7c6f3206b8fdbc24f
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-04-27 07:02:53 -0400

    feat: add route for create and show campground

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app 		= express()
-const path   	= require('path')
-const mongoose = require('mongoose')
-const Campground = require('./model/campground')
+const express 		= require('express')
+const app 				= express()
+const path   			= require('path')
+const mongoose 		= require('mongoose')
+const Campground 	= require('./model/campground')
+const { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } = require('constants')
 
-// mongoose.connect()
 
+mongoose.connect('mongodb+srv://yagnesh:yelpcamp@cluster0.0s9kp.mongodb.net/firstDataBase?retryWrites=true&w=majority',{useNewUrlParser:true, useUnifiedTopology:true})
+				.then(()=>{
+							console.log("successful connection to database")})
+				.catch((err)=>{console.log(`Database connection err: ${err}`) })
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname, 'views'));
@@ -13,11 +20,27 @@ app.set('views',path.join(__dirname, 'views'));
 app.get("/",(req,res)=>{
 	res.render('home')
 })
-
+//index page for campground
 app.get('/campgrounds', async (req,res)=>{
-	// const campgrounds = await Campground.find({})
-	res.render('campgrounds/index')
+	const campgrounds = await Campground.find({})
+	res.render('campgrounds/index',{campgrounds})
 })
+//new campground  form
+app.get("/campgrounds/new",(req,res)=>{
+	res.render('campgrounds/new')
+})
+//post route to create new campground
+app.post("/campgrounds/new", async (req,res)=>{
+	console.log(req.body);
+	const newCampground = await Campground.create(req.body)
+	res.redirect('/campgrounds')
+})
+//single campground show
+app.get('/campgrounds/:id', async (req,res)=>{
+	const campground = await Campground.findById(req.params.id)
+	res.render('campgrounds/show',{campground})
+})
+
 
 //Listening server
 app.listen(3000,()=>{

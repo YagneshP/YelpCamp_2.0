8597c51fe373469831bdd3bbdbccae3ed7ce4f4f
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-04-28 16:13:56 -0400

    feat: add riview model

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.set("views", path.join(__dirname, "views"));
 const validateCampground = (req,res,next) =>{
 	const {error} = validCampgroundSchema.validate(req.body);
 	if(error){
-		const msg = error.details.map(el => el.message.join(','));
+		const msg = error.details.map(el => el.message).join(',');
 		throw new ExpressError(msg, 400)
 	} else{
 		next()

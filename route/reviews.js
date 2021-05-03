f5d46fa74418d156eff071d4f604d290fb009fcb
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-05-03 16:50:38 -0400

    feat: use isLoggedIn middleware for review routes

--- route/reviews.js
+++ route/reviews.js
@@ -5,7 +5,7 @@ const Campground = require("../model/campground");
 const Review = require('../model/review');
 const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
-
+const{isLoggedIn} = require("../middleware")
 const validateReview = (req,res,next) =>{
 	const {error} = validReviewSchema.validate(req.body);
 	if(error){
@@ -16,7 +16,7 @@ const validateReview = (req,res,next) =>{
 	}
 }
 
-router.post('/',validateReview,catchAsync( async(req,res)=>{
+router.post('/',isLoggedIn,validateReview,catchAsync( async(req,res)=>{
 	const campground = await Campground.findById(req.params.id)	
 	const review = await Review.create(req.body.review)
 	campground.reviews.push(review)
@@ -25,7 +25,7 @@ router.post('/',validateReview,catchAsync( async(req,res)=>{
 	res.redirect(`/campgrounds/${campground._id}`)
 }));
 
-router.delete('/:reviewId', catchAsync( async(req,res)=>{
+router.delete('/:reviewId',isLoggedIn, catchAsync( async(req,res)=>{
 	const{id , reviewId} = req.params;
 	await Campground.findByIdAndUpdate(id, {$pull :{reviews: reviewId}});
 	await Review.findByIdAndDelete(reviewId);

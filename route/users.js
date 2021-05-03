2ed094a26dac5829b1f58267b7c543724cf934c7
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-05-03 15:54:28 -0400

    feat: add req.login() method to the register route

--- route/users.js
+++ route/users.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const User = require('../model/user');
 const router = express.Router();
+const catchAsync = require("../utils/catchAsync")
 
 router.get('/register',(req,res)=>{
 	res.render('users/register')
 })
 
-router.post('/register',async (req,res)=>{
+router.post('/register', catchAsync(async (req,res, next)=>{
+try{
 	const {username, email, password} = req.body;
 	// create new User instance with username and email
 	const user = new User({username, email});
 	const newuser =  await User.register(user, password);  //register() method give passportlocalMongoose
-	req.flash('success',"Welcome to the YelpCamp!");
-	res.redirect("/campgrounds"); 
-})
+	req.login(newuser,(err)=>{
+		if(err) return next(err);
+		req.flash('success',"Welcome to the YelpCamp!");
+		res.redirect("/campgrounds");
+	})	 
+} catch(err){
+	req.flash('error', err.message);
+	res.redirect('register')
+}
+
+}));
 
 module.exports = router;

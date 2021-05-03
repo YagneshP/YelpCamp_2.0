5dce6234c26187563b0840a6bd0a1bc6a2bd32f8
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-05-03 15:16:36 -0400

    feat: Add user model

--- model/user.js
+++ model/user.js
@@ -12,6 +12,6 @@ const UserSchema = new Schema({
 
 UserSchema.plugin(passportLocalMongoose);
 
-const User = mongose.model('User', UserSchema);
+const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
